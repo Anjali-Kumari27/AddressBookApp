bf64ab74c9a73c676f2f7b9141ca91a8c06e03d7
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-07 14:18:39 +0530

    [Anjali] : Added modified AddressBook file

--- src/main/java/com/addressbookapp/model/AddressBook.java
+++ src/main/java/com/addressbookapp/model/AddressBook.java
@@ -1,5 +1,7 @@
 package com.addressbookapp.model;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
 	private Contact contact;
@@ -16,7 +18,38 @@ public class AddressBook {
 		if (contact != null) {
 			System.out.println(contact);
 		} else {
-			System.out.println("No contact found in Address Book.");
+			System.out.println("No contact available.");
+		}
+	}
+
+	public void editContact(String firstName) {
+
+		if (contact != null && contact.getFirstName().equals(firstName)) {
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("\nEditing Contact: " + firstName);
+
+			System.out.print("Enter new City: ");
+			String city = scanner.nextLine();
+			contact.setCity(city);
+
+			System.out.print("Enter new State: ");
+			String state = scanner.nextLine();
+			contact.setState(state);
+
+			System.out.print("Enter new Phone Number: ");
+			String phone = scanner.nextLine();
+			contact.setPhoneNumber(phone);
+
+			System.out.print("Enter new Email: ");
+			String email = scanner.nextLine();
+			contact.setEmail(email);
+
+			System.out.println("Contact updated successfully!");
+
+		} else {
+			System.out.println("Contact not found.");
 		}
 	}
 }
