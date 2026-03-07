fd14dc7e5f599b395688200de02399612eafdb06
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-07 15:10:49 +0530

    [Anjali] : Added modified AddressBookTest file

--- src/test/java/com/addressbookapp/AddressBookTest.java
+++ src/test/java/com/addressbookapp/AddressBookTest.java
@@ -9,20 +9,50 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AddressBookTest {
 
 	@Test
-	public void givenContact_whenAddedToAddressBook_shouldReturnSameContact() {
+	public void givenContact_whenDeletedByName_shouldRemoveContact() {
 
 		AddressBook addressBook = new AddressBook();
 
-		Contact contact = new Contact("Anjali", "Singh", "123 MG Road", "Bhopal", "Madhya Pradesh", "462001",
-				"9876543210", "anjali@example.com");
+		Contact contact = new Contact(
+				"Anjali",
+				"Singh",
+				"123 MG Road",
+				"Bhopal",
+				"MP",
+				"462001",
+				"9876543210",
+				"anjali@example.com"
+		);
 
 		addressBook.addContact(contact);
 
-		Contact storedContact = addressBook.getContact();
+		boolean result = addressBook.deleteContact("Anjali");
 
-		assertNotNull(storedContact);
-		assertEquals("Anjali", storedContact.getFirstName());
-		assertEquals("Singh", storedContact.getLastName());
-		assertEquals("Bhopal", storedContact.getCity());
+		assertTrue(result);
+		assertNull(addressBook.getContact());
+	}
+
+	@Test
+	public void givenWrongName_whenDeleted_shouldReturnFalse() {
+
+		AddressBook addressBook = new AddressBook();
+
+		Contact contact = new Contact(
+				"Anjali",
+				"Singh",
+				"123 MG Road",
+				"Bhopal",
+				"MP",
+				"462001",
+				"9876543210",
+				"anjali@example.com"
+		);
+
+		addressBook.addContact(contact);
+
+		boolean result = addressBook.deleteContact("Rahul");
+
+		assertFalse(result);
+		assertNotNull(addressBook.getContact());
 	}
 }
