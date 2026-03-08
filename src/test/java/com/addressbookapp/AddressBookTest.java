351453ebd5c4a7c1000138bfd8eeec0cd0db2da6
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-09 00:17:34 +0530

    [Anjali] : Added modified AddressBookTest file

--- src/test/java/com/addressbookapp/AddressBookTest.java
+++ src/test/java/com/addressbookapp/AddressBookTest.java
@@ -40,24 +40,6 @@ public class AddressBookTest {
 	}
 
 	@Test
-	void givenTwoDifferentContacts_WhenAdded_ShouldReturnTrueForBoth() {
-		AddressBook addressBook = new AddressBook();
-
-		Contact contact1 = new Contact("Anjali", "Singh", "GKP UP", "Gorakhpur", "UP", "273001", "9876543210",
-				"anjali@example.com");
-
-		Contact contact2 = new Contact("Riya", "Sharma", "Civil Lines", "Prayagraj", "UP", "211001", "9123456780",
-				"riya@example.com");
-
-		boolean result1 = addressBook.addContact(contact1);
-		boolean result2 = addressBook.addContact(contact2);
-
-		Assertions.assertTrue(result1);
-		Assertions.assertTrue(result2);
-		Assertions.assertEquals(2, addressBook.getContacts().size());
-	}
-
-	@Test
 	void givenMultipleContacts_WhenSortedByName_ShouldReturnAlphabeticalOrder() {
 		AddressBook addressBook = new AddressBook();
 
@@ -70,5 +52,50 @@ public class AddressBookTest {
 		Assertions.assertEquals("Anjali", sortedContacts.get(0).getFirstName());
 		Assertions.assertEquals("Arpita", sortedContacts.get(1).getFirstName());
 		Assertions.assertEquals("Madhu", sortedContacts.get(2).getFirstName());
+	}
+
+	@Test
+	void givenMultipleContacts_WhenSortedByCity_ShouldReturnCorrectOrder() {
+		AddressBook addressBook = new AddressBook();
+
+		addressBook.addContact(new Contact("Madhu", "Singh", "A", "Kolkata", "WB", "45", "1234", "madhu@mail.com"));
+		addressBook.addContact(new Contact("Anjali", "Singh", "B", "Bhopal", "MP", "34", "2345", "anjali@mail.com"));
+		addressBook.addContact(new Contact("Arpita", "Sharma", "C", "Noida", "UP", "22", "3456", "arpita@mail.com"));
+
+		List<Contact> sortedContacts = addressBook.sortContactsByCity();
+
+		Assertions.assertEquals("Bhopal", sortedContacts.get(0).getCity());
+		Assertions.assertEquals("Kolkata", sortedContacts.get(1).getCity());
+		Assertions.assertEquals("Noida", sortedContacts.get(2).getCity());
+	}
+
+	@Test
+	void givenMultipleContacts_WhenSortedByState_ShouldReturnCorrectOrder() {
+		AddressBook addressBook = new AddressBook();
+
+		addressBook.addContact(new Contact("Madhu", "Singh", "A", "Kolkata", "WB", "45", "1234", "madhu@mail.com"));
+		addressBook.addContact(new Contact("Anjali", "Singh", "B", "Bhopal", "MP", "34", "2345", "anjali@mail.com"));
+		addressBook.addContact(new Contact("Arpita", "Sharma", "C", "Noida", "UP", "22", "3456", "arpita@mail.com"));
+
+		List<Contact> sortedContacts = addressBook.sortContactsByState();
+
+		Assertions.assertEquals("MP", sortedContacts.get(0).getState());
+		Assertions.assertEquals("UP", sortedContacts.get(1).getState());
+		Assertions.assertEquals("WB", sortedContacts.get(2).getState());
+	}
+
+	@Test
+	void givenMultipleContacts_WhenSortedByZip_ShouldReturnCorrectOrder() {
+		AddressBook addressBook = new AddressBook();
+
+		addressBook.addContact(new Contact("Madhu", "Singh", "A", "Kolkata", "WB", "45", "1234", "madhu@mail.com"));
+		addressBook.addContact(new Contact("Anjali", "Singh", "B", "Bhopal", "MP", "34", "2345", "anjali@mail.com"));
+		addressBook.addContact(new Contact("Arpita", "Sharma", "C", "Noida", "UP", "22", "3456", "arpita@mail.com"));
+
+		List<Contact> sortedContacts = addressBook.sortContactsByZip();
+
+		Assertions.assertEquals("22", sortedContacts.get(0).getZip());
+		Assertions.assertEquals("34", sortedContacts.get(1).getZip());
+		Assertions.assertEquals("45", sortedContacts.get(2).getZip());
 	}
 }
