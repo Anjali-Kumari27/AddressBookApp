066538cd2a67deadaaebec164b0f9c770f2e7ab1
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-08 23:53:08 +0530

    [Anjali] : Added modified AddressBookSystemTest file

--- src/test/java/com/addressbookapp/AddressBookSystemTest.java
+++ src/test/java/com/addressbookapp/AddressBookSystemTest.java
@@ -6,13 +6,12 @@ import com.addressbookapp.model.Contact;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 import java.util.Map;
 
 public class AddressBookSystemTest {
 
 	@Test
-	void givenMultipleContacts_WhenViewedByCity_ShouldGroupCorrectly() {
+	void givenMultipleContacts_WhenCountedByCity_ShouldReturnCorrectCounts() {
 		AddressBookSystem system = new AddressBookSystem();
 
 		system.addAddressBook("Friends");
@@ -26,14 +25,14 @@ public class AddressBookSystemTest {
 				new Contact("Arpita", "Sharma", "A2", "Bhopal", "MP", "462002", "67890", "arpita@gmail.com"));
 		family.addContact(new Contact("Riya", "Verma", "A3", "Noida", "UP", "201301", "99999", "riya@gmail.com"));
 
-		Map<String, List<Contact>> cityMap = system.viewPersonsByCity();
+		Map<String, Long> cityCountMap = system.countPersonsByCity();
 
-		Assertions.assertEquals(2, cityMap.get("bhopal").size());
-		Assertions.assertEquals(1, cityMap.get("noida").size());
+		Assertions.assertEquals(2L, cityCountMap.get("bhopal"));
+		Assertions.assertEquals(1L, cityCountMap.get("noida"));
 	}
 
 	@Test
-	void givenMultipleContacts_WhenViewedByState_ShouldGroupCorrectly() {
+	void givenMultipleContacts_WhenCountedByState_ShouldReturnCorrectCounts() {
 		AddressBookSystem system = new AddressBookSystem();
 
 		system.addAddressBook("Friends");
@@ -46,9 +45,9 @@ public class AddressBookSystemTest {
 		office.addContact(new Contact("Rohit", "Kumar", "A2", "Lucknow", "UP", "226001", "67890", "rohit@gmail.com"));
 		office.addContact(new Contact("Pooja", "Sharma", "A3", "Kanpur", "UP", "208001", "77777", "pooja@gmail.com"));
 
-		Map<String, List<Contact>> stateMap = system.viewPersonsByState();
+		Map<String, Long> stateCountMap = system.countPersonsByState();
 
-		Assertions.assertEquals(1, stateMap.get("mp").size());
-		Assertions.assertEquals(2, stateMap.get("up").size());
+		Assertions.assertEquals(1L, stateCountMap.get("mp"));
+		Assertions.assertEquals(2L, stateCountMap.get("up"));
 	}
 }
