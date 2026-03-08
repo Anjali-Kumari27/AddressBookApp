b7f9aa5b5e645862e4d2f39df3d94a57e68d7ae1
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-08 22:33:26 +0530

    [ANjali] : Added modified AddressBookTest file

--- src/test/java/com/addressbookapp/AddressBookTest.java
+++ src/test/java/com/addressbookapp/AddressBookTest.java
@@ -2,42 +2,96 @@ package com.addressbookapp;
 
 import com.addressbookapp.model.AddressBook;
 import com.addressbookapp.model.Contact;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressBookTest {
 
-	@Test
-	public void givenMultipleContacts_whenAdded_shouldStoreAllContacts() {
+    @Test
+    void givenNewContact_WhenAdded_ShouldReturnTrue() {
+        AddressBook addressBook = new AddressBook();
 
-		AddressBook addressBook = new AddressBook();
+        Contact contact = new Contact(
+                "Anjali",
+                "Singh",
+                "GKP UP",
+                "Gorakhpur",
+                "UP",
+                "273001",
+                "9876543210",
+                "anjali@example.com"
+        );
 
-		Contact contact1 = new Contact(
-				"Anjali",
-				"Singh",
-				"123 MG Road",
-				"Bhopal",
-				"Madhya Pradesh",
-				"462001",
-				"9876543210",
-				"anjali@example.com"
-		);
+        boolean result = addressBook.addContact(contact);
 
-		Contact contact2 = new Contact(
-				"Rahul",
-				"Verma",
-				"45 Lake View",
-				"Indore",
-				"Madhya Pradesh",
-				"452001",
-				"9999999999",
-				"rahul@example.com"
-		);
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(1, addressBook.getContacts().size());
+    }
 
-		addressBook.addContact(contact1);
-		addressBook.addContact(contact2);
+    @Test
+    void givenDuplicateContact_WhenAdded_ShouldReturnFalse() {
+        AddressBook addressBook = new AddressBook();
 
-		assertEquals(2, addressBook.getContactList().size());
-	}
+        Contact contact1 = new Contact(
+                "Anjali",
+                "Singh",
+                "GKP UP",
+                "Gorakhpur",
+                "UP",
+                "273001",
+                "9876543210",
+                "anjali@example.com"
+        );
+
+        Contact contact2 = new Contact(
+                "Anjali",
+                "Singh",
+                "Some Other Address",
+                "Lucknow",
+                "UP",
+                "226001",
+                "9999999999",
+                "newmail@example.com"
+        );
+
+        addressBook.addContact(contact1);
+        boolean result = addressBook.addContact(contact2);
+
+        Assertions.assertFalse(result);
+        Assertions.assertEquals(1, addressBook.getContacts().size());
+    }
+
+    @Test
+    void givenTwoDifferentContacts_WhenAdded_ShouldReturnTrueForBoth() {
+        AddressBook addressBook = new AddressBook();
+
+        Contact contact1 = new Contact(
+                "Anjali",
+                "Singh",
+                "GKP UP",
+                "Gorakhpur",
+                "UP",
+                "273001",
+                "9876543210",
+                "anjali@example.com"
+        );
+
+        Contact contact2 = new Contact(
+                "Riya",
+                "Sharma",
+                "Civil Lines",
+                "Prayagraj",
+                "UP",
+                "211001",
+                "9123456780",
+                "riya@example.com"
+        );
+
+        boolean result1 = addressBook.addContact(contact1);
+        boolean result2 = addressBook.addContact(contact2);
+
+        Assertions.assertTrue(result1);
+        Assertions.assertTrue(result2);
+        Assertions.assertEquals(2, addressBook.getContacts().size());
+    }
 }
