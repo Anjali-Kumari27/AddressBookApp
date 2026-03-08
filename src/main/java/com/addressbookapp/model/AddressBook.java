79d8791d38da944783164abe0d34adc39260f339
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-09 00:07:49 +0530

    [Anjali] : Added modified AddressBook file

--- src/main/java/com/addressbookapp/model/AddressBook.java
+++ src/main/java/com/addressbookapp/model/AddressBook.java
@@ -1,40 +1,57 @@
 package com.addressbookapp.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AddressBook {
 
-    private List<Contact> contacts;
+	private List<Contact> contacts;
 
-    public AddressBook() {
-        this.contacts = new ArrayList<>();
-    }
+	public AddressBook() {
+		this.contacts = new ArrayList<>();
+	}
 
-    public boolean addContact(Contact newContact) {
-        boolean isDuplicate = contacts.stream()
-                .anyMatch(existingContact -> existingContact.equals(newContact));
+	public boolean addContact(Contact newContact) {
+		boolean isDuplicate = contacts.stream().anyMatch(existingContact -> existingContact.equals(newContact));
 
-        if (isDuplicate) {
-            return false;
-        }
+		if (isDuplicate) {
+			return false;
+		}
 
-        contacts.add(newContact);
-        return true;
-    }
+		contacts.add(newContact);
+		return true;
+	}
 
-    public void displayContacts() {
-        if (contacts.isEmpty()) {
-            System.out.println("No contacts found.");
-            return;
-        }
+	public List<Contact> getContacts() {
+		return contacts;
+	}
 
-        for (Contact contact : contacts) {
-            System.out.println(contact);
-        }
-    }
+	public void displayContacts() {
+		if (contacts.isEmpty()) {
+			System.out.println("No contacts found.");
+			return;
+		}
 
-    public List<Contact> getContacts() {
-        return contacts;
-    }
+		contacts.forEach(System.out::println);
+	}
+
+	public List<Contact> sortContactsByName() {
+		return contacts.stream()
+				.sorted(Comparator.comparing(Contact::getFirstName, String.CASE_INSENSITIVE_ORDER)
+						.thenComparing(Contact::getLastName, String.CASE_INSENSITIVE_ORDER))
+				.collect(Collectors.toList());
+	}
+
+	public void displaySortedContactsByName() {
+		List<Contact> sortedContacts = sortContactsByName();
+
+		if (sortedContacts.isEmpty()) {
+			System.out.println("No contacts available for sorting.");
+			return;
+		}
+
+		sortedContacts.forEach(System.out::println);
+	}
 }
