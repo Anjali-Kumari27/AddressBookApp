6ccdeb024cd11193ba1bb2e894d5f70626e75bad
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-09 15:25:41 +0530

    [Anjali] : Added modified AddressBookService file

--- src/main/java/com/addressbookapp/service/AddressBookService.java
+++ src/main/java/com/addressbookapp/service/AddressBookService.java
@@ -1,4 +1,9 @@
 package com.addressbookapp.service;
 
+import com.addressbookapp.model.Contact;
+
+import java.util.List;
+
 public interface AddressBookService {
+	List<Contact> getAllContactsFromDB();
 }
