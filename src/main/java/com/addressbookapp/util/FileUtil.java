b00de9d143cc2b24ef15b0627d29ce4fdc79538b
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-09 11:34:16 +0530

    [Anjali] : Added FileUtil file

---
+++ src/main/java/com/addressbookapp/util/FileUtil.java
@@ -0,0 +1,54 @@
+package com.addressbookapp.util;
+
+import com.addressbookapp.model.Contact;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+
+	public static void writeContactsToFile(List<Contact> contacts, String fileName) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+
+			for (Contact contact : contacts) {
+				writer.write(contact.getFirstName() + "," + contact.getLastName() + "," + contact.getAddress() + ","
+						+ contact.getCity() + "," + contact.getState() + "," + contact.getZip() + ","
+						+ contact.getPhoneNumber() + "," + contact.getEmail());
+				writer.newLine();
+			}
+
+			System.out.println("Contacts written to file successfully.");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static List<Contact> readContactsFromFile(String fileName) {
+		List<Contact> contacts = new ArrayList<>();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+				String[] data = line.split(",");
+
+				if (data.length == 8) {
+					Contact contact = new Contact(data[0], data[1], data[2], data[3], data[4], data[5], data[6],
+							data[7]);
+					contacts.add(contact);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return contacts;
+	}
+}
