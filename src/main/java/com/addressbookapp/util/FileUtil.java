cffb66743da90a6ddcb8926706ed3b98a0368fea
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-09 11:46:15 +0530

    [Anjali] : Added modified FileUtil file

--- src/main/java/com/addressbookapp/util/FileUtil.java
+++ src/main/java/com/addressbookapp/util/FileUtil.java
@@ -1,6 +1,8 @@
 package com.addressbookapp.util;
 
 import com.addressbookapp.model.Contact;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -22,7 +24,7 @@ public class FileUtil {
 				writer.newLine();
 			}
 
-			System.out.println("Contacts written to file successfully.");
+			System.out.println("Contacts written to text file successfully.");
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -46,6 +48,49 @@ public class FileUtil {
 			}
 
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return contacts;
+	}
+
+	public static void writeContactsToCSV(List<Contact> contacts, String fileName) {
+		try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
+
+			String[] header = { "FirstName", "LastName", "Address", "City", "State", "Zip", "PhoneNumber", "Email" };
+			writer.writeNext(header);
+
+			for (Contact contact : contacts) {
+				String[] data = { contact.getFirstName(), contact.getLastName(), contact.getAddress(),
+						contact.getCity(), contact.getState(), contact.getZip(), contact.getPhoneNumber(),
+						contact.getEmail() };
+				writer.writeNext(data);
+			}
+
+			System.out.println("Contacts written to CSV file successfully.");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static List<Contact> readContactsFromCSV(String fileName) {
+		List<Contact> contacts = new ArrayList<>();
+
+		try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+			List<String[]> records = reader.readAll();
+
+			for (int i = 1; i < records.size(); i++) {
+				String[] data = records.get(i);
+
+				if (data.length == 8) {
+					Contact contact = new Contact(data[0], data[1], data[2], data[3], data[4], data[5], data[6],
+							data[7]);
+					contacts.add(contact);
+				}
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
