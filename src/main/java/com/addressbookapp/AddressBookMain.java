ba6b244472b435489e7f43466303d549c9464e69
anjaligitlab27 <anjalikumari270704@gmail.com>
2026-03-11 10:29:18 +0530

    [Anjali] : Added modified AddressBookMain file

--- src/main/java/com/addressbookapp/AddressBookMain.java
+++ src/main/java/com/addressbookapp/AddressBookMain.java
@@ -5,20 +5,37 @@ import com.addressbookapp.service.AddressBookService;
 import com.addressbookapp.service.AddressBookServiceImpl;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain {
 
 	public static void start() {
-		System.out.println("Welcome to Address Book Program - UC16");
-
+		Scanner scanner = new Scanner(System.in);
 		AddressBookService addressBookService = new AddressBookServiceImpl();
+
+		System.out.println("Welcome to Address Book Program - UC17");
+
 		List<Contact> contacts = addressBookService.getAllContactsFromDB();
 
-		if (contacts.isEmpty()) {
-			System.out.println("No contacts found in database.");
+		System.out.println("\nContacts retrieved from database:");
+		contacts.forEach(System.out::println);
+
+		System.out.print("\nEnter first name of contact to update phone number: ");
+		String firstName = scanner.nextLine();
+
+		System.out.print("Enter new phone number: ");
+		String newPhoneNumber = scanner.nextLine();
+
+		boolean updated = addressBookService.updateContactPhoneNumber(firstName, newPhoneNumber);
+
+		if (updated) {
+			System.out.println("Contact updated successfully in DB.");
+
+			Contact updatedContact = addressBookService.getContactByFirstName(firstName);
+			System.out.println("\nUpdated contact from DB:");
+			System.out.println(updatedContact);
 		} else {
-			System.out.println("\nContacts retrieved from database:");
-			contacts.forEach(System.out::println);
+			System.out.println("No contact found to update.");
 		}
 	}
 }
